77f925a004fe757d278e781f37b3d33929fa6525
VickyShinny <victoriaoluwaferanmi@gmail.com>
2022-06-14 22:13:29 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="header-class">
+       Basic react application
+      </h1>
+    </div>
+  );
+}
+
+export default App;
